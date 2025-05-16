0a8dc8a9fb60eee5060ff8711d27c85e2afc1283
Justin <joao.ads24@gmail.com>
2025-05-16 15:47:31 -0400

    first commit
    um menu com alguns produtos feitos com react na semana s

--- my-app/app/(paginas)/produtos/page.tsx
+++ my-app/app/(paginas)/produtos/page.tsx
@@ -1,7 +1,28 @@
-export default function Produto(){
+interface ProdutoDados{
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+  category: string;
+  image: string;
+}
+
+
+export default async function Produto(){
+  const resposta = await fetch("https://fakestoreapi.com/products")
+  const dados: ProdutoDados[] = await resposta.json();
+
   return (
     <div>
       <h1>Produto</h1>
+
+      {dados.map(item =>(
+        <div key={item.id}>
+          <h2>{item.title}</h2>
+          <h3>{item.price}</h3>
+        </div>
+      ))}
     </div>
   )
 }
+
--- my-app/componentes/header/index.tsx
+++ my-app/componentes/header/index.tsx
@@ -1,24 +1,26 @@
+import Link from "next/link";
 
+export default function Header() {
+  return (
+    <header className="flex px-2 py-4 bg-violet-400 text-white">
+      <div className="flex items-center justify-between w-full">
+        <div>Menu site</div>
 
-export default function Header(){
-    return (
-        <header className="flex px-2 py-4 bg-violet-400 text-white">
-            <div className="flex items-center justify-between w-full">
-                <div>Menu site</div>
-
-
-                <nav>
-                    <ul className="flex items-center justify-center gap-2">
-                        <li>home</li>
-                        <li>produto</li>
-                    </ul>
-
-                </nav>
-
-
-            </div>
-
-            
-        </header>
-    )
-  }
+        <nav aria-label="Menu principal">
+          <ul className="flex items-center justify-center gap-4">
+            <li>
+              <Link href="/" className="hover:underline">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link href="/produtos" className="hover:underline">
+                Produto
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+}
